72b2c1c9fb3f9abf802de2a960d9fc59773cdbbb
Adel <gerencser.adel@gmail.com>
2022-11-25 12:57:47 +0100

    CategoryItem component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import CategoryItem from './components/category-item/category-item';
 import './categories.styles.scss';
 
 const App = () => {
@@ -5,38 +6,42 @@ const App = () => {
   const categories = [
     {
       id: 1,
-      title: 'Hats'
+      title: 'Hats',
+      imageUrl: "https://images.unsplash.com/photo-1575428652377-a2d80e2277fc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+      /*imageUrl: "https://i.ibb.co/cvpntL1/hats.png"*/
     },
     {
       id: 2,
-      title: 'Jackets'
+      title: 'Sunglasses',
+      imageUrl: "https://images.unsplash.com/photo-1508296695146-257a814070b4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80"
+      /*imageUrl: "https://i.ibb.co/px2tCc3/jackets.png"*/
     },
     {
       id: 3,
-      title: 'Sneakers'
+      title: 'Bags',
+      imageUrl: "https://images.unsplash.com/photo-1630750793250-71173d8bfa5e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+      /*imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png"*/
     },
     {
       id: 4,
-      title: 'Womens'
+      title: 'Women',
+      imageUrl: "https://images.unsplash.com/photo-1616769186373-bc4943852aeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80"
+      /*imageUrl: "https://i.ibb.co/GCCdy8t/womens.png"*/
     },
     {
       id: 5,
-      title: 'Mens'
+      title: 'Men',
+      imageUrl: "https://images.unsplash.com/photo-1532649842991-60f6a04df35d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+      /*imageUrl: "https://i.ibb.co/R70vBrQ/men.png"*/
     }
   ];
 
   return (
     <div className='categories-container'>
 
-      {categories.map( ({id, title}) => (
-        <div className='category-container' key={id}>
-          <div className='background-image'></div>
-          <div className='category-body-container'>
-            <h2>{title}</h2>
-            <p>Shop Now</p>
-          </div>
-      </div>
-    ))}
+      {categories.map( (category) => 
+        <CategoryItem key={category.id} category={category}/>
+      )}
 
     </div>
   )
