7883e93a789dacf7529d324caa21b4e7421d5a69
Adel <gerencser.adel@gmail.com>
2022-12-02 19:10:06 +0100

    authenticating with Firebase

--- src/routes/sign-in/sign-in.jsx
+++ src/routes/sign-in/sign-in.jsx
@@ -1,7 +1,14 @@
+import { signInWithGooglePopup } from '../../utils/firebase/firebase';
+
 const SignIn = () => {
+    const logGoogleUser = async () => {
+        await signInWithGooglePopup()
+    };
+
     return (
         <div>
             <h1>Sign In</h1>
+            <button onClick={logGoogleUser}>Sign In With Google Popup</button>
         </div>
     )
 }
---
+++ src/utils/firebase/firebase.js
@@ -0,0 +1,21 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCBvZ84rZ40oq9orkS0p2ozvuU2BkIH0U4",
+    authDomain: "clothing-ecommerce-db-aefac.firebaseapp.com",
+    projectId: "clothing-ecommerce-db-aefac",
+    storageBucket: "clothing-ecommerce-db-aefac.appspot.com",
+    messagingSenderId: "812256269466",
+    appId: "1:812256269466:web:4c8ec2bfa21e907112fb23"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+    prompt: 'select_account'
+})
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
