7a7a975fb59ae484fe0c7092c02e5a08a1d3fc45
Adel <gerencser.adel@gmail.com>
2022-12-05 15:11:25 +0100

    sign-up-form component

---
+++ src/components/sign-up-form/sign-up-form.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+
+const defaultFormFields = {
+    displayName: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+};
+
+const SignUpForm = () => {
+    const [formField, setFormField] = useState(defaultFormFields);
+    const { displayName, email, password, confirmPassword } = formField;
+
+    console.log(formField)
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+
+        setFormField({...formField, [name]: value})
+    }
+
+    return (
+        <div>
+            <h1>Sign up with your email and password</h1>
+
+            <form onSubmit={() => {}}>
+                <label>Display Name</label>
+                <input type='text' required name='displayName' value={displayName} onChange={handleChange} />
+
+                <label>Email</label>
+                <input type='email' required name='email' value={email} onChange={handleChange} />
+
+                <label>Password</label>
+                <input type='password' required name='password' value={password} onChange={handleChange} />
+
+                <label>Confirm Password</label>
+                <input type='password' required name='confirmPassword' value={confirmPassword} onChange={handleChange} />
+
+                <button type='submit'>Sign Up</button>
+            </form>
+        </div>
+    )
+}
+
+export default SignUpForm;
--- src/routes/sign-in/sign-in.jsx
+++ src/routes/sign-in/sign-in.jsx
@@ -1,4 +1,5 @@
 import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase';
+import SignUpForm from '../../components/sign-up-form/sign-up-form';
 
 const SignIn = () => {
     const logGoogleUser = async () => {
@@ -10,6 +11,7 @@ const SignIn = () => {
         <div>
             <h1>Sign In</h1>
             <button onClick={logGoogleUser}>Sign In With Google Popup</button>
+            <SignUpForm />
         </div>
     )
 }
