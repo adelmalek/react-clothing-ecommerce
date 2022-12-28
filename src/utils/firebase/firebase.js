b94c6e94d45dd5ad2fa7c7053f052cea4b385170
Adel <gerencser.adel@gmail.com>
2022-12-28 19:16:41 +0100

    addCollectionAndDocuments

--- src/utils/firebase/firebase.js
+++ src/utils/firebase/firebase.js
@@ -8,7 +8,14 @@ import {
     signOut,
     onAuthStateChanged,
 } from 'firebase/auth';
-import { getFirestore, doc, getDoc, setDoc} from 'firebase/firestore';
+import { 
+    getFirestore, 
+    doc, 
+    getDoc, 
+    setDoc,
+    collection,
+    writeBatch,
+} from 'firebase/firestore';
 
 const firebaseConfig = {
     apiKey: "AIzaSyCBvZ84rZ40oq9orkS0p2ozvuU2BkIH0U4",
@@ -31,6 +38,19 @@ export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider);
 
 export const db = getFirestore();
+
+export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+    const collectionRef = collection(db, collectionKey);
+    const batch = writeBatch(db);
+
+    objectsToAdd.forEach(object => {
+        const docRef = doc(collectionRef, object.title.toLowerCase());
+        batch.set(docRef, object);
+    });
+
+    await batch.commit();
+    console.log('done');
+};
 
 export const createUserDocumentFromAuth = async (userAuth, additionalInformation = {}) => {
     if (!userAuth) return;
