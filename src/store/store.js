1beeedde210b3999ca5fac8228794e92d85905c9
Adel <gerencser.adel@gmail.com>
2023-01-23 18:04:28 +0100

    redux devtools

--- src/store/store.js
+++ src/store/store.js
@@ -2,23 +2,9 @@ import { compose, applyMiddleware } from 'redux';
 import { legacy_createStore as createStore} from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
-/* import logger from 'redux-logger'; */
+import logger from 'redux-logger'
 
 import { rootReducer } from './root-reducer';
-
-const loggerMiddleware = (store) => (next) => (action) => {
-    if (!action.type) {
-        return next(action)
-    };
-
-    console.log('type: ', action.type);
-    console.log('payload: ', action.payload);
-    console.log('currentState: ', store.getState());
-
-    next(action);
-
-    console.log('next state: ', store.getState());
-};
 
 const persistConfig = {
     key: 'root',
@@ -28,9 +14,14 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWare = [loggerMiddleware];
+const middleWare = [process.env.NODE_ENV !== 'production' && logger].filter(Boolean);
 
-const composedEnhancers = compose(applyMiddleware(...middleWare));
+const composeEnhancer = (process.env.NODE_ENV !== 'production' && 
+    window && 
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+    compose;
+
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWare));
 
 export const store = createStore(persistedReducer, undefined, composedEnhancers);
 
