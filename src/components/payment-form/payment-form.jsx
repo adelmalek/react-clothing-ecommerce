441f03ac2560345f6ace3de3ec3662163fc27b27
Adel <gerencser.adel@gmail.com>
2023-02-09 12:00:06 +0100

    finishing stripe payment

--- src/components/payment-form/payment-form.jsx
+++ src/components/payment-form/payment-form.jsx
@@ -8,18 +8,47 @@ const PaymentForm = () => {
     const stripe = useStripe();
     const elements = useElements();
 
-    const paymentHandler = (e) => {
+    const paymentHandler = async (e) => {
         e.preventDefault();
+
+        if (!stripe || !elements) {
+            return;
+        }
+
+        const response = await fetch('/.netlify/functions/create-payment-intent', {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ amount: 10000 })
+        }).then( res => res.json());
+
+        const { paymentIntent: { client_secret } } = response;
+        
+        const paymentResult = await stripe.confirmCardPayment(client_secret, {
+            payment_method: {
+                card: elements.getElement(CardElement),
+                billing_details: {
+                    name: 'Adel Malek'
+                }
+            }
+        });
+
+        if (paymentResult.error) {
+            alert(paymentResult.error);
+        } else {
+            if (paymentResult.paymentIntent.status === 'succeeded') {
+                alert('Payment Successful');
+            }
+        }
 
     };
 
-    if (!stripe || !elements) {
-        return;
-    }
+    
 
     return (
         <PaymentFormContainer>
-            <FormContainer>
+            <FormContainer onSubmit={paymentHandler}>
                 <h2>Credit Card Payment: </h2>
                 <CardElement/>
                 <Button buttonType={BUTTON_TYPE_CLASSES.inverted}>Pay now</Button>
