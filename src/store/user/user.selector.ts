750ae2110d8beb13efa5acdc0655f1c7f5d30a73
Adel <gerencser.adel@gmail.com>
2023-02-27 17:06:06 +0100

    typing user selector

--- src/store/user/user.selector.ts
+++ src/store/user/user.selector.ts
@@ -1,1 +1,10 @@
-export const selectCurrentUser = (state) => state.user.currentUser;
+import { createSelector } from 'reselect';
+
+import { UserState } from './user.reducer';
+
+export const SelectUserReducer = (state): UserState => state.user;
+
+export const selectCurrentUser = createSelector(
+    SelectUserReducer,
+    (user) => user.currentUser
+);
